80a339ed9c3486af46baf1a67ed9e0c4ea8d8ef7
maakep <schon.fredrik@gmail.com>
2025-12-03 11:32:35 +0100

    xmas(2): there is no way this could have been done with math right? right??

---
+++ aoc25/dec02/p2.js
@@ -0,0 +1,17 @@
+const input = process.argv[4] == "t" ? require("./input-test") : require("./input");
+
+module.exports = () => {
+  const repeatingNumbersRegex = /^(\d+)\1+$/
+  const repeats = [];
+
+  for ({ from, to } of input) {
+    for (let i = from; i <= to; i++) {
+      if (repeatingNumbersRegex.test(i.toString())) {
+        repeats.push(i);
+      }
+    }
+  }
+
+  return repeats.reduce((a, b) => a + b, 0);
+};
+
